27b3d9725da32bb30e6bb4d7fa116c0069149276
pascal <pascal.stavridis@gmail.com>
2012-05-14 11:14:46 +0200

    test1aukeyesjo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Koekjes::Application.config.secret_token = '482a0d6a37fc4786bcb17b8dea0f01f5a98e40a279834c0a846445a9f0e4924d43c18341b26bba775ca2ff3f3e65980f4a756821715e449fb05e4997c545fd54'
